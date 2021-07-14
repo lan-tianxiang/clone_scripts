8011547d92bb50dbc3a6c464a1e25a1a4988057d
root <admin@us.gov>
2021-07-14 04:13:45 -0400

    更新于 07-14 04:13

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,6 +4,8 @@
 #30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
 ##############短期活动##############
+#财富大陆
+45 3,19 * * * node /scripts/jd_wealth_island.js >> /scripts/logs/jd_wealth_island.log 2>&1
 #京东极速版红包(活动时间：2021-5-5至2021-5-31)
 45 0,23 * * * node /scripts/jd_speed_redpocke.js >> /scripts/logs/jd_speed_redpocke.log 2>&1
 #全民抢京豆活动时间：2021-07-02---2021-07-15
